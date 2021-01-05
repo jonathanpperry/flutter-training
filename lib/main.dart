3913e54d837d787d3a5b8370fde51df3e9a61b41
mc-perry <jonathan@m-craft.com>
2021-01-05 19:09:31 +0900

    Completed part 6 video, learning about colours and fonts in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,23 @@ void main() => runApp(MaterialApp(
         appBar: AppBar(
           title: Text("My First App"),
           centerTitle: true,
+          backgroundColor: Colors.red[600],
         ),
         body: Center(
-          child: Text("Hello ninjas!"),
+          child: Text(
+            "Hello ninjas!",
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0,
+                color: Colors.grey[600],
+                fontFamily: "IndieFlower"),
+          ),
         ),
         floatingActionButton: FloatingActionButton(
+          onPressed: () {},
           child: Text("Click"),
+          backgroundColor: Colors.red[600],
         ),
       ),
     ));
