8c7189fb1260330e643b111ddbf12505ab16ad9f
mc-perry <jonathan@m-craft.com>
2021-01-05 18:28:36 +0900

    Completed section 5 to learn about using scaffold and appbar widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Text('Hey ninjas!'),
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text("Hello ninjas!"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text("Click"),
+        ),
+      ),
+    ));
