c2d4bc98ade65ce44f82f19df33177d6cdb3a729
Jonathan Perry <jpper@umich.edu>
2021-01-22 18:55:46 +0900

    Separated main.dart into a stateful widget and its state, in order to call setState to update the UI of the question text being displayed

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Home()));
+void main() => runApp(PersonalityQuizApp());
 
-class Home extends StatelessWidget {
+class PersonalityQuizApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return PersonalityQuizAppState();
+  }
+}
+
+class PersonalityQuizAppState extends State<PersonalityQuizApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("My First App"),
-        centerTitle: true,
+    var questions = [
+      'What\'s your favorite color?',
+      'What\s your favorite animal?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            ElevatedButton(child: Text("Answer 1"), onPressed: answerQuestion),
+            ElevatedButton(child: Text("Answer 2"), onPressed: answerQuestion),
+            ElevatedButton(child: Text("Answer 3"), onPressed: answerQuestion)
+          ],
+        ),
       ),
-      body: Text("This is my default text!"),
     );
   }
 }
