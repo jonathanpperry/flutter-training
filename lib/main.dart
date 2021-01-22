bed03da77fc25fa7ed2737055547857443d3d643
Jonathan Perry <jpper@umich.edu>
2021-01-22 17:42:02 +0900

    Reset app to bare bones app to follow along with Build a Flutter App Crash Course from 3 hours

--- lib/main.dart
+++ lib/main.dart
@@ -9,37 +9,8 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text("My First App"),
         centerTitle: true,
-        backgroundColor: Colors.red[600],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Widget>[Text("Hello,"), Text(" World!")],
-          ),
-          Container(
-            padding: EdgeInsets.all(20.0),
-            color: Colors.pinkAccent,
-            child: Text("one"),
-          ),
-          Container(
-            padding: EdgeInsets.all(30.0),
-            color: Colors.amberAccent,
-            child: Text("two"),
-          ),
-          Container(
-            padding: EdgeInsets.all(40.0),
-            color: Colors.cyan,
-            child: Text("three"),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text("Click"),
-        backgroundColor: Colors.red[600],
-      ),
+      body: Text("This is my default text!"),
     );
   }
 }
